b50c44c9428ab10522c43d90fa7537613248aebf
acodenil <anilvaishnav.5251@gmail.com>
2022-12-10 11:59:02 +0530

    dzsftg

--- index.php
+++ index.php
@@ -5,11 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .box{
+            height:50px;
+            width:50px;
+        }
+    </style>
 </head>
 <body>  
     jkhadsf
     <?php
     echo "strar";
     ?>
+    <div class="box">ZSDf</div>
 </body>
 </html>
